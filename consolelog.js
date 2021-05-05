e2accf6e31471a02f41c618038dee9bcd329f8e5
RAFhub6 <72543405+RAFhub6@users.noreply.github.com>
2021-05-05 17:32:48 +1000

    Create consolelog.js

---
+++ consolelog.js
@@ -0,0 +1,5 @@
+console.log(Math.floor(Math.random() * 1000000000000000000000000))
+console.log(This is your PureX Admin Code (PXAC)
+console.log("font-size:50px;" , "WARNING : "
+console.log("anyone that has your PXAC has full access to your account! if someone told you to paste something here , dont do it!"
+console.log("it will let them have full administrative access to your PureX NCS Account. if you dont know what your doing here just close this tool without even knowing")
